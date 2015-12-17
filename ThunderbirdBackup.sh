34649501f7c03a59aa271806d7dad3a29c3b17ac
Vitaliy Orlov <orlov0562@gmail.com>
2015-12-17 14:33:32 +0200

    One script instance logic added. Autorestarting thunderbird disabled due to problems in Kubuntu

--- ThunderbirdBackup.sh
+++ ThunderbirdBackup.sh
@@ -2,7 +2,7 @@
 
 # -----------------------------------------------------------------------
 # Thunderbird profile backup script
-# See more information at: 
+# See more information at:
 # http://www.it-rem.ru/perenos-profilya-thunderbird-na-drugoy-disk.html
 # -----------------------------------------------------------------------
 
@@ -12,23 +12,46 @@ BACKUP_FOLDER=/store/Backups/Thunderbird
 THUNDERBIRD_PROFILE_PATH=/store/Software/Thunderbird/biiyroo1.default
 BACKUP_FILENAME=thunderbird-profile-`date +"%Y_%m_%d"`
 ATTEMPTS_TO_STOP_THUNDERBIRD=5
-LEAVE_FILES_AFTER_CLEAN_UP=3
+LEAVE_FILES_AFTER_CLEAN_UP=5
 BACKUP_COMPRESSION_RATE=6
 RESTART_THUNDERBIRD_IF_RUNNED=1
 IS_THUNDERBIRD_RUNNED=0
+LOCK_FILE=/tmp/thunderbird-backup.lock
+
+# -----------------------------------------------------------------------
+
+# Check for only one copy of the script running
+
+# create empty lock file if none exists
+cat /dev/null >> $LOCK_FILE
+read lastPID < $LOCK_FILE
+
+# if lastPID is not null and a process with that pid exists , exit
+[ ! -z "$lastPID" -a -d /proc/$lastPID ] && exit
+
+# not running, save my pid in the lock file
+echo $$ > $LOCK_FILE
+
+echo "Starting.."
 
 # -----------------------------------------------------------------------
 
 # Check if we already have backup for period
 
-if [ "$1" != "force" ]; then
-	if [[ $(find $BACKUP_FOLDER -type f -print -quit) ]]; then
-		LAST_BACKUP_FILE=$( find $BACKUP_FOLDER -type f -print0 | xargs -0 ls -lt | grep -v '^total' | awk '{print $(NF)}' | head -n 1)
-		LAST_BACKUP_FILE_DATE=$(date -r $LAST_BACKUP_FILE '+%Y%m%d')
-		LAST_SAT_DATE=$(date -d "last-saturday" '+%Y%m%d')
-		# If last backup date greater than last saturday then exit
-		if [ $LAST_BACKUP_FILE_DATE -ge $LAST_SAT_DATE ]; then exit; fi
-	fi
+if [ "$1" != "force" ];
+then
+    if [[ $(find $BACKUP_FOLDER -type f -print -quit) ]];
+    then
+        LAST_BACKUP_FILE=$( find $BACKUP_FOLDER -type f -print0 | xargs -0 ls -lt | grep -v '^total' | awk '{print $(NF)}' | head -n 1)
+        LAST_BACKUP_FILE_DATE=$(date -r $LAST_BACKUP_FILE '+%Y%m%d')
+        LAST_SAT_DATE=$(date -d "last-saturday" '+%Y%m%d')
+        # If last backup date greater than last saturday then exit
+        if [ $LAST_BACKUP_FILE_DATE -ge $LAST_SAT_DATE ];
+        then
+            echo "Fresh backup already exists, exiting.."
+            exit;
+        fi
+    fi
 fi
 
 # -----------------------------------------------------------------------
@@ -39,28 +62,31 @@ fi
 
 for ((i=1; i<=$ATTEMPTS_TO_STOP_THUNDERBIRD; i++))
 do
-	if pgrep "thunderbird" > /dev/null;	then
+    if pgrep "thunderbird" > /dev/null;
+    then
 
-		IS_THUNDERBIRD_RUNNED=1
+        IS_THUNDERBIRD_RUNNED=1
 
-		if [ $i -eq 1 ]; then
-			MSG="I want to do regular backup."
-		else
-			MSG="Thunderbird still running."
-		fi
+        if [ $i -eq 1 ]; then
+                MSG="I want to do regular backup."
+        else
+                MSG="Thunderbird still running."
+        fi
 
-		zenity \
-			--question \
-			--title="Thunderbird backup" \
-			--text="$MSG\nCould you please close Thunderbird?" \
-			--display=:0.0 
+        echo "$MSG. Could you please close Thunderbird?"
 
-		# if No btn pressed exit
-		if [ $? -eq 1 ]; then exit; fi
+        zenity \
+                --question \
+                --title="Thunderbird backup" \
+                --text="$MSG\nCould you please close Thunderbird?" \
+                --display=:0.0
 
-	else
-		break
-	fi
+        # if No btn pressed exit
+        if [ $? -eq 1 ]; then exit; fi
+
+    else
+            break
+    fi
 done
 
 
@@ -69,8 +95,9 @@ done
 
 if pgrep "thunderbird" > /dev/null
 then
-	zenity --error --text "Can't do backup - Thunderbird still running.." --display=:0.0 
-	exit
+    zenity --error --text "Can't do backup - Thunderbird still running.." --display=:0.0
+    echo "Error: Can't do backup - Thunderbird still running..";
+    exit
 fi
 
 # -----------------------------------------------------------------------
@@ -81,24 +108,25 @@ fi
 pushd $THUNDERBIRD_PROFILE_PATH > /dev/null
 cd ..
 zip -$BACKUP_COMPRESSION_RATE \
-	-r "$BACKUP_FOLDER/$BACKUP_FILENAME.zip" \
-	-q \
-	`basename $THUNDERBIRD_PROFILE_PATH`
+    -r "$BACKUP_FOLDER/$BACKUP_FILENAME.zip" \
+    -q \
+    `basename $THUNDERBIRD_PROFILE_PATH`
 popd > /dev/null
 
 # -----------------------------------------------------------------------
 
 # Clean up old archives
-if [[ $(find $BACKUP_FOLDER -type f -print -quit) ]]; then
-	FILES_LIST=$(find $BACKUP_FOLDER -type f -print0 | xargs -0 ls -lt | grep -v '^total' | awk '{print $(NF)}')
-	COUNTER=$LEAVE_FILES_AFTER_CLEAN_UP
+if [[ $(find $BACKUP_FOLDER -type f -print -quit) ]];
+then
+    FILES_LIST=$(find $BACKUP_FOLDER -type f -print0 | xargs -0 ls -lt | grep -v '^total' | awk '{print $(NF)}')
+    COUNTER=$LEAVE_FILES_AFTER_CLEAN_UP
 
-	for FILE in $FILES_LIST
-	do
-		((COUNTER--));	
-		if [ $COUNTER -ge 0 ]; then continue; fi
-		rm $FILE
-	done
+    for FILE in $FILES_LIST
+    do
+            ((COUNTER--));
+            if [ $COUNTER -ge 0 ]; then continue; fi
+            rm $FILE
+    done
 fi
 # -----------------------------------------------------------------------
 
@@ -106,9 +134,11 @@ fi
 
 if [ -f "$BACKUP_FOLDER/$BACKUP_FILENAME.zip" ];
 then
-	notify-send "Thunderbird backup created"
+    echo "Thunderbird backup successfully created"
+    notify-send "Thunderbird backup successfully created"
 else
-	zenity --error --text "Thunderbird backup hasn't created\nUndefined error happend" --display=:0.0 
+    echo "Error: Thunderbird backup hasn't created. Undefined error happend"
+    zenity --error --text "Thunderbird backup hasn't created\nUndefined error happend" --display=:0.0
 fi
 
 # -----------------------------------------------------------------------
@@ -116,8 +146,14 @@ fi
 # Start thunderbird if it running when script starts
 # and user shutdown it just for backup
 
-if [ $IS_THUNDERBIRD_RUNNED -eq 1 ]; then
-	if [ $RESTART_THUNDERBIRD_IF_RUNNED -eq 1 ]; then
-		/usr/bin/thunderbird &
-	fi
+if [ $IS_THUNDERBIRD_RUNNED -eq 1 ];
+then
+    if [ $RESTART_THUNDERBIRD_IF_RUNNED -eq 1 ];
+    then
+        echo "Now you can rerun thunderbird if needed"
+        # In this place you can start thunderbird, smth like:
+        # thunderbird &
+        # unfortunately in my Kubuntu this command starts
+        # thunderbird with empty profile, so I disabled it
+    fi
 fi
